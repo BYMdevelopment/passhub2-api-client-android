dc9758a67044ce6a545603470dfc55a88c45a05d
dreamfire <denisykkpi@gmail.com>
2019-08-16 19:58:09 +0300

    removed idea files

--- app/src/main/java/com/bymdev/passhub2sdk/MainActivity.kt
+++ app/src/main/java/com/bymdev/passhub2sdk/MainActivity.kt
@@ -1,12 +1,7 @@
 package com.bymdev.passhub2sdk
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import com.bymdev.pass2sdk.Pass2SDK
-import com.bymdev.pass2sdk.core.CallbackWrapper
-import com.bymdev.pass2sdk.core.PassError
-import com.bymdev.pass2sdk.model.response.AuthResponse
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,18 +11,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Pass2SDK(this.applicationContext)
-            .onSignIn("dreamer1", "dreamer")
-            .subscribeWith(object : CallbackWrapper<List<AuthResponse>>() {
-                override fun onSuccess(result: List<AuthResponse>) {
-                    Log.d(LOG_TAG, "onSuccess = $result")
-                }
-
-                override fun onError(error: PassError) {
-                    Log.d(LOG_TAG, "onSuccess = $error")
-                }
-
-            })
 
     }
 
