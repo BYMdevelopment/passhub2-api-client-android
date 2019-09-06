dabf0dadd2fbae89cac52e120bc2f1b945db1574
dreamfire <denisykkpi@gmail.com>
2019-09-06 19:29:56 +0300

    make request parameters optional for validation request body

--- pass2sdk/src/main/java/com/bymdev/pass2sdk/model/request/ValidationRequestBody.kt
+++ pass2sdk/src/main/java/com/bymdev/pass2sdk/model/request/ValidationRequestBody.kt
@@ -1,5 +1,5 @@
 package com.bymdev.pass2sdk.model.request
 
 data class ValidationRequestBody(val voucherCode: String,
-                                 val sku: String,
-                                 val voucherProductId: Int)
+                                 val sku: String?,
+                                 val voucherProductId: Int?)
