e442d2993b56fd759918db6452b0911ba544456e
dreamfire <denisykkpi@gmail.com>
2019-09-06 15:52:00 +0300

    change validation response model

--- pass2sdk/src/main/java/com/bymdev/pass2sdk/model/response/validate/Customer.kt
+++ pass2sdk/src/main/java/com/bymdev/pass2sdk/model/response/validate/Customer.kt
@@ -2,8 +2,9 @@ package com.bymdev.pass2sdk.model.response.validate
 
 data class Customer(
 	val uid: String? = null,
-	val firstName: Any? = null,
-	val lastName: Any? = null,
-	val phoneNumber: Any? = null,
-	val email: Any? = null
+	val firstName: String? = null,
+	val lastName: String? = null,
+	val phoneNumber: String? = null,
+	val email: String? = null,
+	val products: List<Product>? = null
 )
