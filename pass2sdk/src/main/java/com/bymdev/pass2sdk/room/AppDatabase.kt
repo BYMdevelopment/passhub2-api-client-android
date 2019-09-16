add65857933f4c30f572845b9e2d8c5cd8e0877e
dreamfire <denisykkpi@gmail.com>
2019-09-16 13:35:28 +0300

    add migration database

--- pass2sdk/src/main/java/com/bymdev/pass2sdk/room/AppDatabase.kt
+++ pass2sdk/src/main/java/com/bymdev/pass2sdk/room/AppDatabase.kt
@@ -8,18 +8,26 @@ import com.bymdev.pass2sdk.room.dao.AccountDao
 import com.bymdev.pass2sdk.room.entity.AccountEntity
 
 
-@Database(entities = [AccountEntity::class], version = 1)
+@Database(entities = [AccountEntity::class], version = 3)
 abstract class AppDatabase : RoomDatabase() {
 
+    /**
+     * We use fallbackToDestructiveMigration
+     * to clear database every time when
+     * version has been upgraded
+     */
+//    TODO remove fallbackToDestructiveMigration and provide migration scheme before release
     companion object {
-        val DB_NAME = "pass_sdk_database.db"
+        private val DB_NAME = "pass_sdk_database.db"
         private var INSTANCE: AppDatabase? = null
 
         fun getInstance(context: Context): AppDatabase {
             if (INSTANCE == null) {
                 synchronized(AppDatabase::class) {
                     INSTANCE = Room.databaseBuilder(context.applicationContext,
-                        AppDatabase::class.java, DB_NAME).build()
+                        AppDatabase::class.java, DB_NAME)
+                        .fallbackToDestructiveMigration()
+                        .build()
                     return INSTANCE!!
                 }
             } else {
